99ea6062abf4ee3b70e30c6127e7fb29d05bcd5f
asitroyt <asitroy1628@gmail.com>
2025-03-21 18:42:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KHAN-MD~72JFQK6T#nacW5KMIDUeYRqjZ23UzumW8pYA9OI7ggJi93q_K8dc",
+SESSION_ID: process.env.SESSION_ID || "Try WhatsApp on your computer https://web.whatsapp.com/",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
